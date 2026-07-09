6fa2248751f6c4faefaa045c120f6519c269fce1
Batistuta91 <batistuta91@walla.co.il>
2026-07-09 15:37:30 +0300

    1

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.cidev.datecodedecoder',
-  appName: 'מפענח תאריך ייצור',
-  webDir: 'www'
-};
-
-export default config;
