31a3362f0ecf9ff8b184413c52bebe214f7f1d63
Batistuta91 <batistuta91@walla.co.il>
2026-07-09 15:24:52 +0300

    NEW

---
+++ android/app/src/main/java/com/cidev/datecodedecoder/MainActivity.java
@@ -0,0 +1,5 @@
+package com.cidev.datecodedecoder;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cidev.datecodedecoder',
+  appName: 'מפענח תאריך ייצור',
+  webDir: 'www'
+};
+
+export default config;
